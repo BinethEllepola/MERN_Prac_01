cdb130fcbe513c46645968fdb904a7d70bf6d130
Bineth <binethellepola@email.com>
2025-07-27 22:28:09 +0530

    Connect the Mongodb Connection

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,1 +1,18 @@
-console.log("bye");
+//pass - OHIc1SKNmmpdIoXv
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use("/",(req, res, next)=>{
+    res.send("It is working");
+
+})
+
+mongoose.connect("mongodb+srv://admin:OHIc1SKNmmpdIoXv@cluster0.ysesjrj.mongodb.net/")
+.then(()=> console.log("Connecterd mongo BD"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log((err)));
